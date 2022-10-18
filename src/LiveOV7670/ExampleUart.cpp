64501e6c85d59d96c8337d6861ca51a5805abd42
Vasili <34661066+vasing@users.noreply.github.com>
2022-10-18 19:38:31 +0200

    all code in ExampleUart.cpp (verified)

--- src/LiveOV7670/ExampleUart.cpp
+++ src/LiveOV7670/ExampleUart.cpp
@@ -7,6 +7,9 @@
 #if EXAMPLE == 3
 #include "Arduino.h"
 #include "CameraOV7670.h"
+#include "Adafruit_DRV2605.h"
+#include "MyTCA9548A.h"
+
 
 #define CONTRAST 255    //0...255 Setzt OV7670 Kontrast. 
                         //64 = default
@@ -14,6 +17,31 @@
                         //0 = default
 //Tislenko: Parameter fuer Graustufen Limit in Funktion formatPixelByteBinary()
 const uint8_t BinaryLimit = 150; // Graustufen Limit
+
+
+
+Adafruit_DRV2605 drv;
+MyTCA9548A TCA(0x70);
+void DRVcalib(int I2CPort);
+void sentGo(int I2CPort);
+
+// change setup.h to switch between buffered and pixel-by-pixel processing
+
+
+//Tislenko
+#define LRA_SW 1        //0 - Schaltet LRA Ansteuerung aus
+                        //1 - Schaltet LRA Ansteuerung ein
+#define UART_INIT 0     //0 - Schaltet UART ArduinoCapture aus
+                        //1 - Schaltet UART ArduinoCapture ein
+#define UART_OUTPUT 0   //0 - Schaltet UART Output durch ArduinoCapture aus
+                        //1 - Schaltet UART Output durch ArduinoCapture ein
+
+
+uint16_t MIN_LINE_WIDTH = 10;
+uint16_t MAX_LINE_WIDTH = 100;
+
+uint16_t DetectLineWidth();
+void ResetData();
 
 
 // select resolution and communication speed:
@@ -54,6 +82,8 @@ uint8_t ROW_0 = 0b00000000;
 uint8_t ROW_1 = 0b00000000;
 uint8_t ROW_2 = 0b00000000;
 uint8_t ROW_3 = 0b00000000;
+
+
 
 int LINE_0_FRAME[159];      //Reihe 1, Frame 1
 int LINE_1_FRAME[159];      //Reihe 2, Frame 1
@@ -344,7 +374,7 @@ void initializeScreenAndCamera() {
         sendBlankFrame(COLOR_RED);
         delay(3000);
 #endif
-
+        
 //Wenn Kontrast manuell definiert und nicht auskommentiert
 //#ifdef CONTRAST
         camera.setContrast(CONTRAST);
@@ -357,6 +387,12 @@ void initializeScreenAndCamera() {
 
         //Delay, sonnst ist if() leer
         delay(100);
+
+        DRVcalib(0);    //calibration for DVR on TCA port 0
+        DRVcalib(1);    //calibration for DVR on TCA port 1
+        DRVcalib(2);    //calibration for DVR on TCA port 2
+        DRVcalib(3);    //calibration for DVR on TCA port 3
+
     }
 
 }
@@ -382,6 +418,9 @@ void sendBlankFrame(uint16_t color) {
 
 // this is called in Arduino loop() function
 void processFrame() {
+    
+
+    
     processedByteCountDuringCameraRead = 0;
     //todo Konfigurierbar machen zwischen Normalbetrieb und ArduinoCapture
     commandStartNewFrame(uartPixelFormat);
@@ -395,6 +434,49 @@ void processFrame() {
     Serial.println(frameCounter);
     
     //commandDebugPrint("Frame " + String(frameCounter, 16)); // send number in hexadecimal
+
+#if LRA_SW==1
+
+
+    //if ((DetectLineWidth() > MIN_LINE_WIDTH) && (DetectLineWidth() > MAX_LINE_WIDTH)) {
+    //    sentGo(0);
+    //}
+
+    //Serial.println("link");
+    //sentGo(0);      //LRA "links" (Zeigefinger)
+    //delay(1000);
+    //Serial.println("unten");
+
+    //sentGo(1);      //sent DVR waveform trigger on oprt 6
+    //delay(1000);
+    //Serial.println("rechts");
+
+    //sentGo(2);      //sent DVR waveform trigger on oprt 6
+    //delay(1000);
+    //Serial.println("oben");
+
+    //sentGo(3);      //sent DVR waveform trigger on oprt 6
+    //delay(1000);
+#endif
+
+    /**/
+    //#if UART_ONLY==1
+      Serial.println("link");
+      sentGo(0);      //sent DVR waveform trigger on oprt 7
+      delay(1000);
+      Serial.println("unten");
+      sentGo(1);      //sent DVR waveform trigger on oprt 6
+      delay(1000);
+      Serial.println("rechts");
+      sentGo(2);      //sent DVR waveform trigger on oprt 6
+      delay(1000);
+      Serial.println("oben");
+      sentGo(3);      //sent DVR waveform trigger on oprt 6
+      delay(1000);
+    //#endif
+    
+
+
 }
 
 
@@ -743,4 +825,57 @@ void setBinaryResult(uint8_t value, uint16_t nr) {
     }
 }
 
+
+uint16_t DetectLineWidth() {
+
+    uint8_t LineWidth = 0;
+
+    //uint8_t gap = AllowedGap;
+
+    for (int i = 0; i <= 160; i++) {            //#Durchlaufe Frame Linie
+        if (LINE_0_FRAME[i] == 1) {              //#Suche erstes Pixel
+            //FirstDetected = true;               //#DetectLine Start
+            LineWidth++;                        //#zähle Linienbreite
+        }
+    }
+    return LineWidth;
+}
+
+void ResetData() {
+
+    for (int i = 0; i <= 159; i++) {
+        LINE_0_FRAME[i] = 0;
+        LINE_1_FRAME[i] = 0;
+    }
+}
+
+
+void DRVcalib(int I2CPort) {
+
+    TCA.resetChannel();
+    TCA.setChannel(I2CPort);
+
+    drv.begin();
+    drv.useLRA();
+    //drv.useERM();
+    // I2C trigger by sending 'go' command 
+    drv.setMode(DRV2605_MODE_INTTRIG); // default, internal trigger when sending GO command
+
+    drv.selectLibrary(1);
+    drv.setWaveform(0, 82);  // ramp up medium 1, see datasheet part 11.2
+    drv.setWaveform(1, 1);  // strong click 100%, see datasheet part 11.2
+    drv.setWaveform(2, 0);  // end of waveforms
+}
+
+void sentGo(int I2CPort) {
+    TCA.resetChannel();
+    TCA.setChannel(I2CPort);
+
+    drv.begin();
+    drv.go();
+}
+
+
 #endif
+
+
--- src/LiveOV7670/LiveOV7670.ino
+++ src/LiveOV7670/LiveOV7670.ino
@@ -1,31 +1,9 @@
 
 
 #include <Wire.h>
-#include "Adafruit_DRV2605.h"
-#include "MyTCA9548A.h"
 
-Adafruit_DRV2605 drv;
-MyTCA9548A TCA(0x70);
-void DRVcalib(int I2CPort);
-void sentGo(int I2CPort);
 
-// change setup.h to switch between buffered and pixel-by-pixel processing
 #include "setup.h"
-
-//Tislenko
-#define LRA_SW 1        //0 - Schaltet LRA Ansteuerung aus
-                        //1 - Schaltet LRA Ansteuerung ein
-#define UART_INIT 0     //0 - Schaltet UART ArduinoCapture aus
-                        //1 - Schaltet UART ArduinoCapture ein
-#define UART_OUTPUT 0   //0 - Schaltet UART Output durch ArduinoCapture aus
-                        //1 - Schaltet UART Output durch ArduinoCapture ein
-
-
-int MIN_LINE_WIDTH = 10;
-int MAX_LINE_WIDTH = 100;
-
-uint16_t DetectLineWidth();
-void ResetData();
 
 
 void setup() {
@@ -39,10 +17,7 @@ void setup() {
   //Serial.begin(9600);
   
 
-  DRVcalib(0);    //calibration for DVR on TCA port 0
-  DRVcalib(1);    //calibration for DVR on TCA port 1
-  DRVcalib(2);    //calibration for DVR on TCA port 2
-  DRVcalib(3);    //calibration for DVR on TCA port 3
+
 }
 
 
@@ -55,95 +30,8 @@ void loop() {
     
     //Serial.println("loop");
 
-#if LRA_SW==1
-    
-    
-    if ((DetectLineWidth > MIN_LINE_WIDTH) && (DetectLineWidth > MAX_LINE_WIDTH)) {
-        sentGo(0);
-    }
-    
-    //Serial.println("link");
-    sentGo(0);      //LRA "links" (Zeigefinger)
-    //delay(1000);
-    //Serial.println("unten");
-    
-    //sentGo(1);      //sent DVR waveform trigger on oprt 6
-    //delay(1000);
-    //Serial.println("rechts");
-    
-    //sentGo(2);      //sent DVR waveform trigger on oprt 6
-    //delay(1000);
-    //Serial.println("oben");
-    
-    //sentGo(3);      //sent DVR waveform trigger on oprt 6
-    //delay(1000);
-#endif
-
-/*
-#if UART_ONLY==1
-  Serial.println("link");
-  sentGo(0);      //sent DVR waveform trigger on oprt 7
-  delay(1000);
-  Serial.println("unten");
-  sentGo(1);      //sent DVR waveform trigger on oprt 6
-  delay(1000);
-  Serial.println("rechts");
-  sentGo(2);      //sent DVR waveform trigger on oprt 6
-  delay(1000);
-  Serial.println("oben");
-  sentGo(3);      //sent DVR waveform trigger on oprt 6
-  delay(1000);
-#endif
-*/
 }
 
 
 
-void DRVcalib(int I2CPort) {
 
-    TCA.resetChannel();
-    TCA.setChannel(I2CPort);
-
-    drv.begin();
-    drv.useLRA();
-    //drv.useERM();
-    // I2C trigger by sending 'go' command 
-    drv.setMode(DRV2605_MODE_INTTRIG); // default, internal trigger when sending GO command
-
-    drv.selectLibrary(1);
-    drv.setWaveform(0, 82);  // ramp up medium 1, see datasheet part 11.2
-    drv.setWaveform(1, 1);  // strong click 100%, see datasheet part 11.2
-    drv.setWaveform(2, 0);  // end of waveforms
-}
-
-void sentGo(int I2CPort) {
-    TCA.resetChannel();
-    TCA.setChannel(I2CPort);
-
-    drv.begin();
-    drv.go();
-}
-
-uint16_t DetectLineWidth() {
-
-    uint8_t LineWidth = 0;
-
-    //uint8_t gap = AllowedGap;
-
-    for (int i = 0; i <= 160; i++) {            //#Durchlaufe Frame Linie
-        if (LINE_0_FRAME[i] == 1) {              //#Suche erstes Pixel
-            //FirstDetected = true;               //#DetectLine Start
-            LineWidth++;                        //#zähle Linienbreite
-        }
-    }
-    return LineWidth;
-}
-
-void ResetData() {
-
-    for (int i = 0; i <= 159; i++) {
-        LINE_0_FRAME[i] = 0;
-        LINE_1_FRAME[i] = 0;
-        LINE_RESULT[i] = 0;
-    }
-}
